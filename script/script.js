17533d2848e51b6dc5c10dc57a45f3c00b26d007
Richard Borges <ribborges@outlook.com>
2022-02-10 22:43:09 -0300

    Menu update

--- script/script.js
+++ script/script.js
@@ -1,14 +1,16 @@
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function openMenu() {
-    var x = document.getElementById("navbar");
+    var x = document.getElementById("menu");
     
     if (x.style.opacity == 0) {
         x.style.opacity = 1;
         x.style.zIndex = 1;
+        x.style.display = "flex";
     }
     
     else {
         x.style.opacity = 0;
         x.style.zIndex = -1;
+        x.style.display = "none";
     }
 }
