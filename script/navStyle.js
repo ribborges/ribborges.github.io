a4edec94babb77302ecfd8048d0d8a1709492c6a
Richard Borges <Richard>
2022-07-23 19:42:13 -0300

    Novo visual desenvolvido com SCSS e JavaScript

---
+++ script/navStyle.js
@@ -0,0 +1,22 @@
+const header = document.querySelector("nav");
+const topSection = document.querySelector(".header");
+const navElem = document.getElementById("navbar");
+const navBtns = navElem.children;
+
+const options = {
+    rootMargin: "-200px 0px 0px 0px"
+};
+
+const topSectionObserver = new IntersectionObserver(function(entries, topSectionObserver) {
+    entries.forEach(entry => {
+        if(!entry.isIntersecting) {
+            navElem.classList.add("scrolled");
+        }
+
+        else {
+            navElem.classList.remove("scrolled");
+        }
+    })
+}, options);
+
+topSectionObserver.observe(topSection);
