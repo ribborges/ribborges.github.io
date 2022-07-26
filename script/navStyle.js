6122273bedc3c97086b7bda53b58bcb447248534
Richard Borges <Richard>
2022-07-26 17:10:18 -0300

    Adição do 'accordion' para o portfolio

--- script/navStyle.js
+++ script/navStyle.js
@@ -20,3 +20,22 @@ const topSectionObserver = new IntersectionObserver(function(entries, topSection
 }, options);
 
 topSectionObserver.observe(topSection);
+
+var acc = document.getElementsByClassName("accordion-btn");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
